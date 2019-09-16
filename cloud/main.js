d9de80e57976a0c30263eb9db8b36b4f58fbf288
hellfire_my <hellfire_my@yahoo.com>
2019-09-16 21:46:24 +0800

    Lets try implement something interesting

--- cloud/main.js
+++ cloud/main.js
@@ -1,3 +1,18 @@
 Parse.Cloud.define('hello', function(req, res) {
   return 'Hi';
 });
+
+Parse.Cloud.define('login', function(req, res) {
+	const username = req.params.username;
+	const password = req.params.password;
+	const user = await Parse.User.logIn(username, password);
+	const sessionToken = user.getSessionToken();
+	const oldSessionQuery = new Parse.Query(Parse.Session);
+	
+	oldSessionQuery.equalTo('user', user);
+	oldSessionQuery.notEqualTo('sessionToken', sessionToken);
+	
+	const oldSession = await oldSessionsQuery.find({ useMasterKey: true });
+	await Parse.Object.destroyAll(oldSession, { useMasterKey: true });
+	return sessionToken;
+});
