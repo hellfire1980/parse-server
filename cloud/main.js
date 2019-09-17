253b204bc9de80bfa9ea9ed3664ea4228ac0a096
hellfire_my <hellfire_my@yahoo.com>
2019-09-17 14:56:36 +0800

    Revert changes

--- cloud/main.js
+++ cloud/main.js
@@ -2,7 +2,7 @@ Parse.Cloud.define('hello', function(req, res) {
   return 'Hi';
 });
 
-Parse.Cloud.define('login', function(req, res) {
+/*Parse.Cloud.define('login', function(req, res) {
 	const username = req.params.username;
 	const password = req.params.password;
 	const user = await Parse.User.logIn(username, password);
@@ -15,4 +15,4 @@ Parse.Cloud.define('login', function(req, res) {
 	const oldSession = await oldSessionsQuery.find({ useMasterKey: true });
 	await Parse.Object.destroyAll(oldSession, { useMasterKey: true });
 	return sessionToken;
-});
+});*/
