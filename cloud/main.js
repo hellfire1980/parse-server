004ca6ac1530980accd6f331a62bcaf2bfdef907
hellfire_my <hellfire_my@yahoo.com>
2019-10-29 11:04:57 +0800

    !) Add auth sucscription webhook

--- cloud/main.js
+++ cloud/main.js
@@ -14,7 +14,7 @@ Parse.Cloud.define('login', async (request) => {
 	const id = user.id;
 	const sessionToken = user.getSessionToken();
 	const vip = user.get("vip");
-	const trialDate = user.get("trialDate");
+	const subscribeDate = user.get("subscribeDate");
 	const oldSessionsQuery = new Parse.Query(Parse.Session);
 	
 	oldSessionsQuery.equalTo('user', user);
@@ -28,7 +28,91 @@ Parse.Cloud.define('login', async (request) => {
         "sessionToken": sessionToken,
 		"objectId": id,
 		"vip": vip,
-		"trialDate": trialDate
+		"subscribeDate": subscribeDate
     };
+	return jsonObject;
+});
+
+Parse.Cloud.define('authSubscription', async (request) => {
+	const order_id = request.params.order_id;
+	const product_title = request.params.product_title;
+	const product_id = request.params.product_id;
+	const buyer_email = request.params.buyer_email;
+	const buyer_ip = request.params.buyer_ip;
+	const payment_method = request.params.payment_method; //0 = Paypal, 1 = Bitcoin, 2 = Ethereum, 3 = Perfect Money, 4 = Stripe
+	const invoice_amount = request.params.invoice_amount;
+	const invoice_amount_usd = request.params.invoice_amount_usd;
+	const quantity = request.params.quantity;
+	const purchased_at = request.params.purchased_at;
+	const txn_id = request.params.txn_id;
+	const status = request.params.status;
+	const custom_fields = JSON.parse(request.params.custom_fields);
+	const invoice_id = request.params.invoice_id;
+	const email = custom_fields.Email_to_subscribe;
+	
+	var todayDate;
+	var calExpireDate;
+	var vip = "";
+	var monthVal = 0;
+	var remark;
+	
+	if (product_id == 'ad524b6771ff') {
+		vip = "monthly";
+		monthVal = 1;
+	}
+	
+	if (product_id == 'ad03f62ba4a5') {
+		vip = "yearly";
+		monthVal = 12;
+	}
+	
+	if ((status == 3 || status == 4 ) && vip != '') {
+		const userQuery = new Parse.Query(Parse.User);	
+		
+		userQuery.equalTo('email', email);
+		
+		const user = await userQuery.first({ useMasterKey: true });
+		
+		if (user != undefined) {
+			var expireDate = user.get("expireDate")
+			
+			todayDate = new Date(new Date().toUTCString());
+			
+			if (expireDate == undefined) {
+				calExpireDate = new Date(todayDate.setTime(todayDate.getMonth + monthVal));
+				user.set("expireDate", calExpireDate);
+			} else {
+				calExpireDate = new Date(expireDate.setTime(expireDate.getMonth + monthVal));
+				user.set("expireDate", calExpireDate);
+			}
+			
+			user.set("vip", vip);
+			user.set("subscribeDate", todayDate);
+			user.save(null, { useMasterKey: true, 
+				success: function(data) {
+					response.success();
+				},
+				error: function(error) {
+					response.error(error.message);
+				}
+			});
+		} else {
+			remark = "User email not exists";
+		}
+		
+	}
+	
+	var jsonObject = {
+		"product_id": product_id,
+        "email": email,
+		"purchased_at": purchased_at,
+		"payment_method": payment_method,
+		"invoice_amount_usd": invoice_amount_usd,
+		"vip": vip,
+		"subscribeDate": todayDate,
+		"expireDate": calExpireDate,
+		"remark": remark
+    };
+	
 	return jsonObject;
 });
--- index.js
+++ index.js
@@ -4,6 +4,7 @@
 var express = require('express');
 var ParseServer = require('parse-server').ParseServer;
 var path = require('path');
+var unirest = require('unirest');
 
 var databaseUri = process.env.DATABASE_URI || process.env.MONGODB_URI;
 
@@ -58,8 +59,24 @@ app.get('/', function(req, res) {
 
 // There will be a test page available on the /test path of your server url
 // Remove this before launching your app
-app.get('/test', function(req, res) {
-  res.sendFile(path.join(__dirname, '/public/test.html'));
+//app.get('/test', function(req, res) {
+//  res.sendFile(path.join(__dirname, '/public/test.html'));
+//});
+
+// -------------------
+//	Open subscription
+// -------------------
+// This is a post methos without authentication require
+app.use(express.json());
+
+app.post('/authSubscription', function(req, res) {
+	 unirest.post("http://localhost:1337/parse/functions/authSubscription")
+    .headers({'X-Parse-Application-Id': process.env.APP_ID, 'Content-Type': 'application/json'})
+	.send(req.body)
+    .end(function(response) {
+		console.log(response.body);
+		res.status(200).send();
+     });
 });
 
 var port = process.env.PORT || 1337;
